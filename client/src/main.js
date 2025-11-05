1a9f64887c70782b06d8162a6f51bd9e387cecf2
cn27529 <cn27529@hotmail.com>
2025-11-05 15:51:23 +0800

    ./改@/ ../改@/

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { authService } from "@/services/authService";
 
-import App from "./App.vue";
+import App from "@/App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import "@/style.css";
