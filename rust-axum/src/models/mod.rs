4e7a89ab809e7e5233e010690399144f0a0fba80
cn27529 <cn27529@hotmail.com>
2026-04-23 10:06:45 +0800

    remove participation_record.rs

--- rust-axum/src/models/mod.rs
+++ rust-axum/src/models/mod.rs
@@ -2,7 +2,6 @@ pub mod activity;
 pub mod registration;
 pub mod api_response;  // 新增這行
 pub mod monthly_donate;
-pub mod participation_record;
 pub mod my_data;
 pub mod receipt_number; // ✅ 新增：收據編號模型
 pub mod directus_users; 
